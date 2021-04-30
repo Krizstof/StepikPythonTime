201a1891f218964e59445e4c55bba8ced985ad16
Krizstof <vozniakkrizstof@yandex.ru>
2021-04-30 02:37:32 -0400

    free

---
+++ lastch2.py
@@ -0,0 +1,26 @@
+dict = {}
+zdict= {}
+
+string = input()
+chstring = input()
+fstring = input()
+zstring = input()
+
+for i in range(len(string)):
+    if string[i] not in dict:
+        dict[string[i]]=chstring[i]
+for i in range(len(string)):
+     if chstring[i] not in zdict:
+        zdict[chstring[i]]=string[i]
+
+print(dict)
+print(zdict)
+
+for i in range(len(fstring)):
+   print(dict[fstring[i]],end="")
+
+for  i in range(len(zstring)):
+    if i == 0:
+        print('\n',zdict[zstring[i]],end="")
+    else:
+        print(zdict[zstring[i]],end="")
